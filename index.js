a013c56a1c08133f870343e645750508e224f1b1
preshi zankat <preshitazankat@gmail.com>
2025-01-26 09:36:37 +0530

    Add production build

--- index.js
+++ index.js
@@ -4,22 +4,43 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+const allowedOrigins = [
+  'https://blog-client-9gw84kx6i-preshi-zankats-projects.vercel.app'
+];
+app.use(cors({
+   origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the HTTP methods you need
+  credentials: true,
+}));
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-  res.status(404).json({ error: 'Route not found' });
-});
+
 
 // Routes
 app.use("/api/posts", require("./routes/post"));
+
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
 
 // Server setup
 const PORT = process.env.PORT || 5000;
