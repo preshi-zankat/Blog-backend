58ae339a644e2d99d75a9adfedf9030bd5088278
preshi zankat <preshitazankat@gmail.com>
2025-01-26 08:26:09 +0530

    change index.js

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 // Routes
 app.use("/api/posts", require("./routes/post"));
