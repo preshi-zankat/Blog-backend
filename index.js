7e117eccaf6bdee81ada1a74704acad4f866eaf2
preshita zankat <94054515+preshi-zankat@users.noreply.github.com>
2025-01-26 10:06:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
