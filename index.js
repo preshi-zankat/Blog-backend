0a166334e23b98f18fa11ec79916df1c78e7215a
preshita zankat <94054515+preshi-zankat@users.noreply.github.com>
2025-01-30 16:40:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 // CORS configuration
 const allowedOrigins = [
   'https://blog-client-k3ryfz2fi-preshi-zankats-projects.vercel.app',
-  'https://blog-client-git-main-preshi-zankats-projects.vercel.app'
+  'https://blog-client-git-main-preshi-zankats-projects.vercel.app',
+  'https://api.unsplash.com/search/photos?query=${query}&client_id=qHoSgW1oMKc5Pz_YbpEtmOxwfz1BaYH4tx-vAV3eSI8'
 ];
 
 app.use(cors({
